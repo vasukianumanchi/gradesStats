4028edf803627123e5cd2ab39f97a960c3dad6d4
Vasuki <vasuki.k@gmail.com>
2018-05-09 15:01:35 -0400

    Initial Commit

---
+++ GradeBook.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class GradeBook
+    {
+        List<float> grades;
+        public GradeBook()
+        {
+            grades = new List<float>();
+        }
+
+        public GradeStatistics computeStatistics()
+        {
+            GradeStatistics stats =  new GradeStatistics();
+            stats.HighestGrade = 0;
+            float sum = 0;
+            foreach(float grade in grades)
+            {
+               
+                    stats.HighestGrade = Math.Max(grade,stats.HighestGrade);
+                    stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+                    sum = sum + grade;
+            }
+            stats.AverageGrade = sum / grades.Count;
+            return stats;
+        }
+       
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+
+            GradeBook gBook = new GradeBook();
+            gBook.AddGrade(91);
+            gBook.AddGrade(89.5f);
+            gBook.AddGrade(75);
+
+           GradeStatistics stats = gBook.computeStatistics();
+            Console.WriteLine("Average Grade is '" + stats.AverageGrade+"'");
+            Console.WriteLine("Lowest Grade is '" + stats.LowestGrade+"'");
+            Console.WriteLine("Highest Grade is '" + stats.HighestGrade+"'");
+           
+
+
+
+        }
+    }
+}
