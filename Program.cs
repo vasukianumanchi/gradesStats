276ece049e4639c4fe600c27d025a99337eabcc7
Vasuki <vasuki.k@gmail.com>
2018-05-10 15:44:28 -0400

    added speech synthesizer assembly

--- Program.cs
+++ Program.cs
@@ -3,27 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Speech.Synthesis;
 
 namespace Grades
 {
     class Program
     {
         static void Main(string[] args)
-        { 
-
+        {
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            synth.Speak("Hello! This is a great book program");
             GradeBook gBook = new GradeBook();
             gBook.AddGrade(91);
             gBook.AddGrade(89.5f);
             gBook.AddGrade(75);
-
-           GradeStatistics stats = gBook.computeStatistics();
+            GradeStatistics stats = gBook.computeStatistics();
             Console.WriteLine("Average Grade is '" + stats.AverageGrade+"'");
             Console.WriteLine("Lowest Grade is '" + stats.LowestGrade+"'");
             Console.WriteLine("Highest Grade is '" + stats.HighestGrade+"'");
-           
-
-
-
         }
     }
 }
